774fe1e9a1db08922ee75ffe587c43e5871e13a7
sunlei99 <355890577@qq.com>
2014-08-20 14:43:29 +0800

    coding

--- rediscounter.c
+++ rediscounter.c
@@ -48,24 +48,255 @@ int add_aof(Aof * aof_obj, char * item){
 
 unsigned char read_byte(FILE * fp){
     unsigned char number;
-    if (fread_check(&number, 1, 1, fp) == 0) return -1;
+    if (fread(&number, 1, 1, fp) == 0) return -1;
     return number;
 }
+
 
 long long rdb_get_long(FILE * fp){
     long long number;
     unsigned char digits = read_byte(fp);
     if(digits > RDB_INVALID_LEN)
         return -1;
-    if (fread_check(&number, digits, 1, fp) == 0) return -1;
+    if (fread(&number, digits, 1, fp) == 0) return -1;
     return number;
 }
 
+/* Load an encoded length from the DB, see the REDIS_RDB_* defines on the top
+ * of this file for a description of how this are stored on disk.
+ *
+ * isencoded is set to 1 if the readed length is not actually a length but
+ * an "encoding type", check the above comments for more info */
+uint32_t rdbLoadLen(FILE *fp, int *isencoded) {
+    unsigned char buf[2];
+    uint32_t len;
+    int type;
+
+    if (isencoded) *isencoded = 0;
+    if (fread(buf,1,1,fp) == 0) return REDIS_RDB_LENERR;
+    type = (buf[0]&0xC0)>>6;
+    if (type == REDIS_RDB_6BITLEN) {
+        /* Read a 6 bit len */
+        return buf[0]&0x3F;
+    } else if (type == REDIS_RDB_ENCVAL) {
+        /* Read a 6 bit len encoding type */
+        if (isencoded) *isencoded = 1;
+        return buf[0]&0x3F;
+    } else if (type == REDIS_RDB_14BITLEN) {
+        /* Read a 14 bit len */
+        if (fread(buf+1,1,1,fp) == 0) return REDIS_RDB_LENERR;
+        return ((buf[0]&0x3F)<<8)|buf[1];
+    } else {
+        /* Read a 32 bit len */
+        if (fread(&len,4,1,fp) == 0) return REDIS_RDB_LENERR;
+        return ntohl(len);
+    }
+}
+
+/* Load an integer-encoded object from file 'fp', with the specified
+ * encoding type 'enctype'. If encode is true the function may return
+ * an integer-encoded object as reply, otherwise the returned object
+ * will always be encoded as a raw string. */
+sds rdbLoadIntegerObject(FILE *fp, int enctype, int encode) {
+    unsigned char enc[4];
+    long long val;
+
+    if (enctype == REDIS_RDB_ENC_INT8) {
+        if (fread(enc,1,1,fp) == 0) return NULL;
+        val = (signed char)enc[0];
+    } else if (enctype == REDIS_RDB_ENC_INT16) {
+        uint16_t v;
+        if (fread(enc,2,1,fp) == 0) return NULL;
+        v = enc[0]|(enc[1]<<8);
+        val = (int16_t)v;
+    } else if (enctype == REDIS_RDB_ENC_INT32) {
+        uint32_t v;
+        if (fread(enc,4,1,fp) == 0) return NULL;
+        v = enc[0]|(enc[1]<<8)|(enc[2]<<16)|(enc[3]<<24);
+        val = (int32_t)v;
+    } else {
+        val = 0; /* anti-warning */
+        fprintf(stderr, "Unknown RDB integer encoding type");
+        exit(1);
+        return NULL;
+    }
+
+    return sdsfromlonglong(val);
+}
+
+
+sds rdbLoadLzfStringObject(FILE*fp) {
+    unsigned int len, clen;
+    unsigned char *c = NULL;
+    sds val = NULL;
+
+    if ((clen = rdbLoadLen(fp,NULL)) == REDIS_RDB_LENERR) return NULL;
+    if ((len = rdbLoadLen(fp,NULL)) == REDIS_RDB_LENERR) return NULL;
+    if ((c = zmalloc(clen)) == NULL) goto err;
+    if ((val = sdsnewlen(NULL,len)) == NULL) goto err;
+    if (fread(c,clen,1,fp) == 0) goto err;
+    //lzf_decompress to be added
+    //if (lzf_decompress(c,clen,val,len) == 0) goto err;
+    zfree(c);
+    return val;
+err:
+    zfree(c);
+    sdsfree(val);
+    return NULL;
+}
+
+sds rdbGenericLoadStringObject(FILE*fp, int encode) {
+    int isencoded;
+    uint32_t len;
+    sds val;
+
+    len = rdbLoadLen(fp,&isencoded);
+    if (isencoded) {
+        switch(len) {
+            case REDIS_RDB_ENC_INT8:
+            case REDIS_RDB_ENC_INT16:
+            case REDIS_RDB_ENC_INT32:
+                return rdbLoadIntegerObject(fp,len,encode);
+            case REDIS_RDB_ENC_LZF:
+                return rdbLoadLzfStringObject(fp);
+            default:
+                fprintf(stderr, "Unknown RDB encoding type");
+                return NULL;
+        }
+    }
+
+    if (len == REDIS_RDB_LENERR) return NULL;
+    val = sdsnewlen(NULL,len);
+    if (len && fread(val,len,1,fp) == 0) {
+        sdsfree(val);
+        return NULL;
+    }
+    return val;
+}
+
+
+sds rdbLoadStringObject(FILE *fp) {
+    return rdbGenericLoadStringObject(fp,0);
+}
+
+sds rdbLoadEncodedStringObject(FILE *fp) {
+    return rdbGenericLoadStringObject(fp,1);
+}
+
+/* For information about double serialization check rdbSaveDoubleValue() */
+int rdbLoadDoubleValue(FILE *fp, double *val) {
+    char buf[128];
+    unsigned char len;
+
+    if (fread(&len,1,1,fp) == 0) return -1;
+    if(len > 252){
+        fprintf(stderr, "wrong number in rdbLoadDoubleValue");
+        return 0;
+    }
+    if (fread(buf,len,1,fp) == 0) return -1;
+    buf[len] = '\0';
+    sscanf(buf, "%lg", val);
+    return 0;
+
+}
+
+/* check if value is prime, return 1 if it is, 0 otherwise */
+int _isPrime(unsigned long value)
+{
+    unsigned long i;
+    if (value <= 1)
+        return 0;
+    if (value == 2)
+        return 1;
+    if (value % 2 == 0)
+        return 0;
+
+    for (i = 3; i < sqrt(value) + 1; ++i)
+        if (value % i == 0)
+            return 0;
+
+    return 1;
+}
+
+int init_rdb_state(rdb_state * state, FILE * fp){
+    char buf[1024];
+    int rdbver;
+    sds sdstemp;
+    double val;
+
+    if (fread(buf,9,1,fp) == 0) goto eoferr;
+    buf[9] = '\0';
+    if (memcmp(buf,"REDIS",5) != 0) {
+        fclose(fp);
+        fprintf(stderr,"Wrong signature trying to load DB from file");
+        return COUNTER_ERR;
+    }
+    /*-------redis version check-------*/
+    rdbver = atoi(buf+5);
+    if (rdbver >= 2 && rdbver <= 4) {
+        if (!(sdstemp = rdbLoadStringObject(fp)) || rdbLoadDoubleValue(fp, (double *)&(state->offset))) {
+            fclose(fp);
+            fprintf(stderr, "Failed to get aof name or offset");
+            return COUNTER_ERR;
+        }
+        char aofname[255];
+        sprintf(aofname, "%s", sdstemp);
+        state->rdb_filename = strdup(aofname);
+        fprintf(stdout, "RDB position: %s-%lld",
+                    state->rdb_filename, state->offset);
+    } else if (rdbver != 1) {
+        fclose(fp);
+        fprintf(stderr, "Can't handle RDB format version %d",rdbver);
+        return COUNTER_ERR;
+    }
+
+    fprintf(stdout, "Loading dict information...");
+    /* load db size, used, deleted_slots, key_size, entry_size */
+    if (rdbLoadDoubleValue(fp, &val) == -1) goto eoferr;
+    state->size = (unsigned long)val;
+    if (rdbLoadDoubleValue(fp, &val) == -1) goto eoferr;
+    state->used = (unsigned long)val;
+    if (rdbLoadDoubleValue(fp, &val) == -1) goto eoferr;
+    state->deleted = (unsigned long)val;
+    if (rdbLoadDoubleValue(fp, &val) == -1) goto eoferr;
+    state->key_size = (unsigned int)val;
+    if (rdbLoadDoubleValue(fp, &val) == -1) goto eoferr;
+    state->entry_size = (unsigned int)val;
+    fprintf(stdout,  "RDB info: size-%lld, used-%lld, deleted-%lld, key-size-%lld, "
+                "entry-size-%lld", state->size, state->used, state->deleted, state->key_size, state->entry_size);
+    state->value_size = state->entry_size - state->key_size;
+
+    // check if values are valid
+    if (!_isPrime(state->size) ||
+        state->used > state->size ||
+        state->deleted > state->size ||
+        state->key_size < 1)
+    {
+        fprintf(stderr, "Invalid values got from RDB: size-%lld, "
+                "used-%lld, deleted-%lld, key_size-%lld",
+                state->size, state->used, state->deleted, state->key_size);
+        fprintf(stderr, "Error loading from DB. Aborting now.");
+        exit(1);
+        return COUNTER_ERR;
+    }
+
+    sdsfree(sdstemp);
+    return COUNTER_OK;
+eoferr:
+    sdsfree(sdstemp);
+    fprintf(stderr, "init_rdb_state failed");
+    return COUNTER_ERR;
+}
+
+
+
+
+
 //default format kv, need to free result, is it ok?
 char * _format_kv(char *key, int value){
-    char *result = (char *)malloc(sizeof(char) * (strlen(key) + strlen(value) + 2));
-    sprintf(result, "%s:%d", key, value);
-    return result;
+    char tmp[1024];
+    sprintf(tmp, "%s:%d", key, value);
+    return strdup(tmp);
 }
 
 int _key_hash(char * key, int value){
@@ -76,11 +307,11 @@ int _key_hash(char * key, int value){
 rdb_state state;
 
 void adjust_block_size(long long entry_size){
-    printf(("REDISCOUNTER_RDB_BLOCK=%d", REDISCOUNTER_RDB_BLOCK));
+    fprintf(stdout, "REDISCOUNTER_RDB_BLOCK=%lld", REDISCOUNTER_RDB_BLOCK);
     REDISCOUNTER_RDB_BLOCK = entry_size * ((REDISCOUNTER_RDB_BLOCK + entry_size - 1) / entry_size);
-    printf(("REDISCOUNTER_RDB_BLOCK=%d", REDISCOUNTER_RDB_BLOCK));
+    fprintf(stdout, "REDISCOUNTER_RDB_BLOCK=%lld", REDISCOUNTER_RDB_BLOCK);
 }
-
+/*
 void rdb_get_dict(){
     adjust_block_size(state.entry_size);
     long long total = state.size * state.entry_size,
@@ -102,7 +333,7 @@ void rdb_get_dict(){
         if(i < count){
             key_count = REDISCOUNTER_RDB_BLOCK / state.entry_size;
             nkey_read = REDISCOUNTER_RDB_BLOCK;
-            if (fread_check(buf,REDISCOUNTER_RDB_BLOCK,1,fp) == 0) {
+            if (fread(buf,REDISCOUNTER_RDB_BLOCK,1,fp) == 0) {
                 sdsfree(val);
                 //report error
             }
@@ -110,7 +341,7 @@ void rdb_get_dict(){
         else{
             key_count = rest / self.entry_size;
             nkey_read = rest;
-            if (fread_check(buf,rest,1,fp) == 0) {
+            if (fread(buf,rest,1,fp) == 0) {
                 sdsfree(val);
                 //report error
             }
@@ -121,3 +352,4 @@ void rdb_get_dict(){
         }
     }
 }
+*/
--- rediscounter.h
+++ rediscounter.h
@@ -1,6 +1,15 @@
 #ifndef REDISCOUNTER_H
 #define REDISCOUNTER_H
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <arpa/inet.h> // for ntohl
+#include <limits.h>
+#include <math.h>
+#include "sds.h"
+#include "zmalloc.h"
+
 //defination of key-value formatting
 typedef char * format_kv(void * key, int key_len, long value);
 typedef struct rdb_state{
@@ -11,7 +20,43 @@ typedef struct rdb_state{
     long long key_size;
     long long entry_size;
     long long value_size;
-    char * rdb_filename;
+    sds rdb_filename;
 }rdb_state;
+
+#define RDB_INVALID_LEN 252
+#define AOF_BUFFER_SIZE 10240
+
+// return state
+#define COUNTER_ERR -1
+#define COUNTER_OK 1
+
+/* Defines related to the dump file format. To store 32 bits lengths for short
+ * keys requires a lot of space, so we check the most significant 2 bits of
+ * the first byte to interpreter the length:
+ *
+ * 00|000000 => if the two MSB are 00 the len is the 6 bits of this byte
+ * 01|000000 00000000 =>  01, the len is 14 byes, 6 bits + 8 bits of next byte
+ * 10|000000 [32 bit integer] => if it's 01, a full 32 bit len will follow
+ * 11|000000 this means: specially encoded object will follow. The six bits
+ *           number specify the kind of object that follows.
+ *           See the REDIS_RDB_ENC_* defines.
+ *
+ * Lenghts up to 63 are stored using a single byte, most DB keys, and may
+ * values, will fit inside. */
+#define REDIS_RDB_6BITLEN 0
+#define REDIS_RDB_14BITLEN 1
+#define REDIS_RDB_32BITLEN 2
+#define REDIS_RDB_ENCVAL 3
+#define REDIS_RDB_LENERR UINT_MAX
+
+
+/* When a length of a string object stored on disk has the first two bits
+ * set, the remaining two bits specify a special encoding for the object
+ * accordingly to the following defines: */
+// encoding state
+#define REDIS_RDB_ENC_INT8 0        /* 8 bit signed integer */
+#define REDIS_RDB_ENC_INT16 1       /* 16 bit signed integer */
+#define REDIS_RDB_ENC_INT32 2       /* 32 bit signed integer */
+#define REDIS_RDB_ENC_LZF 3         /* string compressed with FASTLZ */
 
 #endif // REDISCOUNTER_H
